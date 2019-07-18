f64d2ffebf010b4aedbc2c7c79e69699323bbd89
Pavel Korolev <dev@borodust.org>
2019-07-18 14:43:04 +0300

    Switch to method verification in tests

--- src/test/java/org/borodust/likeit/service/impl/LikeServiceTest.java
+++ src/test/java/org/borodust/likeit/service/impl/LikeServiceTest.java
@@ -6,12 +6,8 @@ import org.borodust.likeit.service.LikeService;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.concurrent.atomic.AtomicLong;
-
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 /**
  * @author Pavel Korolev
@@ -20,29 +16,26 @@ public class LikeServiceTest {
     private static final String THING_TO_LIKE = "like-it-service";
     private LikeService likeService;
     private Fairy fairy = Fairy.create();
+    private LikeRepository likeRepository;
 
     @Before
     public void cleanupFixture() {
-        this.likeService = new DefaultLikeService(setupLikeRepositoryMock());
-    }
-
-    private LikeRepository setupLikeRepositoryMock() {
-        LikeRepository mock = mock(LikeRepository.class);
-        AtomicLong counter = new AtomicLong(0);
-        doAnswer((invocation) -> counter.incrementAndGet())
-                .when(mock).increment(THING_TO_LIKE);
-        when(mock.count(THING_TO_LIKE))
-                .thenAnswer((invocation) -> counter.get());
-        return mock;
+        this.likeRepository = mock(LikeRepository.class);
+        this.likeService = new DefaultLikeService(likeRepository);
     }
 
     @Test
     public void like() {
-        long currentLikes = likeService.getLikes(THING_TO_LIKE);
-
         likeService.like(THING_TO_LIKE);
 
-        assertEquals(currentLikes + 1, likeService.getLikes(THING_TO_LIKE));
+        verify(likeRepository).increment(THING_TO_LIKE);
+    }
+
+    @Test
+    public void getLikes() {
+        likeService.getLikes(THING_TO_LIKE);
+
+        verify(likeRepository).count(THING_TO_LIKE);
     }
 
     @Test(expected = IllegalArgumentException.class)
